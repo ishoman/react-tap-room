0387ac0fc9c80243f11b109246b9528010dea4d3
Isaac Shoman <isaac.shoman@gmail.com>
2018-06-29 15:44:17 -0700

    Remove test image from error page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -6,7 +6,6 @@ function Error404(props){
   return (
     <div>
       <h2>The page {props.location.pathname} does not exist!</h2>
-      <img src={test}/>
       <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,6 @@
 import KegList from './KegList/KegList';
 import React from 'react';
-import Buoy from './../assets/images/buoy.jpg';
-import Burnside from './../assets/images/burnsidebrewing.jpg';
-import Crux from './../assets/images/crux.jpg';
-import Heater from './../assets/images/heaterallen.png';
+
 
 
 
