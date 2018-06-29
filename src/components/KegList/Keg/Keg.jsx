fcfd4dece6d3bea6c8a6e76d0e68a4eddee36926
Isaac Shoman <isaac.shoman@gmail.com>
2018-06-29 13:21:40 -0700

    Return from lunch and make more components

---
+++ src/components/KegList/Keg/Keg.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Keg() {
+  return (
+    <div>
+      <KegImage/>
+      <KegInfo/>
+      <SellPint/>
+      <EditKeg/>
+    </div>
+  );
+}
---
+++ src/components/KegList/Keg/KegItems/SellPint.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Keg() {
+  return (
+    <div>
+
+    </div>
+
+  );
+}
